898607bfaa015a9462716dfb5442226e74444775
firozkhanirl <firozkhanirl@gmail.com>
2018-08-14 23:03:25 +0530

    input data format pojo

---
+++ daily.trade.report/src/main/java/firoz/daily/trade/report/TradeData.java
@@ -0,0 +1,16 @@
+package firoz.daily.trade.report;
+
+import java.util.Date;
+
+public class TradeData {
+
+	String entity;
+	String action;
+	double agreedFx;
+	String currency;
+	Date instructionDate;	
+	Date settlementDate;
+	int units;
+	double price;
+	
+}
