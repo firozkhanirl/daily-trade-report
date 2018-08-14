1e091ebfd4eed7e7fd2c6c8153e953f33b33a28c
firozkhanirl <firozkhanirl@gmail.com>
2018-08-14 22:52:54 +0530

    creating blank maven project

---
+++ daily.trade.report/src/main/java/firoz/daily/trade/report/App.java
@@ -0,0 +1,13 @@
+package firoz.daily.trade.report;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
