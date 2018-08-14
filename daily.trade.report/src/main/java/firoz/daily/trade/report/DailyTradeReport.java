ccb69d744eb0bdac895eecbb660b9edd5aa30cf8
firozkhanirl <firozkhanirl@gmail.com>
2018-08-14 23:10:33 +0530

    able to print object from class. removed main method

--- daily.trade.report/src/main/java/firoz/daily/trade/report/DailyTradeReport.java
+++ daily.trade.report/src/main/java/firoz/daily/trade/report/DailyTradeReport.java
@@ -1,14 +1,26 @@
 package firoz.daily.trade.report;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
+
+import firoz.daily.trade.report.TradeData;
+
 
 public class DailyTradeReport 
 {
 	static Logger logger = Logger.getLogger(DailyTradeReport.class);
+
+	/**
+	   * This method should read input records and do further operation
+	   */
+	public void processInputData(List<TradeData> dataList) {
+		logger.info("Processing Input Data.............");
+		
+		for(TradeData data : dataList) {
+			logger.debug(data);
+		}
+	}
 	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        logger.debug("first logger message");
-    }
+	
 }
--- daily.trade.report/src/test/java/firoz/daily/trade/report/DailyTradeReportTest.java
+++ daily.trade.report/src/test/java/firoz/daily/trade/report/DailyTradeReportTest.java
@@ -1,38 +1,29 @@
 package firoz.daily.trade.report;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class DailyTradeReportTest 
-    extends TestCase
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class DailyTradeReportTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public DailyTradeReportTest( String testName )
-    {
-        super( testName );
+	static SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy"); 		// 	02-Jan-2016
+	
+	@Test
+    public void testProcessorWithTwoRecords() throws ParseException {
+		List<TradeData> dataList = new ArrayList<TradeData>();
+		dataList.add( new TradeData("foo", "B", 0.50, "SGP", sdf.parse("01-Jan-2016"), sdf.parse("02-Jan-2016"), 200, 100.25));
+		dataList.add( new TradeData("bar", "S", 0.22, "AED", sdf.parse("05-Jan-2016"), sdf.parse("07-Jan-2016"), 450, 150.5));
+		
+		DailyTradeReport processor = new DailyTradeReport();
+		processor.processInputData(dataList);
+		
+		assertTrue( true );
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( DailyTradeReportTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	
 }
