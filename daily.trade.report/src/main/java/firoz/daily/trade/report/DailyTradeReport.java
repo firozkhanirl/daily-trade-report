f42663fe107add418a294cc9541263c765a38739
firozkhanirl <firozkhanirl@gmail.com>
2018-08-14 23:01:49 +0530

    log4j properties

--- daily.trade.report/src/main/java/firoz/daily/trade/report/DailyTradeReport.java
+++ daily.trade.report/src/main/java/firoz/daily/trade/report/DailyTradeReport.java
@@ -1,13 +1,14 @@
 package firoz.daily.trade.report;
 
-/**
- * Hello world!
- *
- */
+import org.apache.log4j.Logger;
+
 public class DailyTradeReport 
 {
+	static Logger logger = Logger.getLogger(DailyTradeReport.class);
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        logger.debug("first logger message");
     }
 }
