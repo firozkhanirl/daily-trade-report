6568ac6d931aed3e09a725b54a3833a141ebe090
firozkhanirl <firozkhanirl@gmail.com>
2018-08-14 23:03:59 +0530

    getter setter

--- daily.trade.report/src/main/java/firoz/daily/trade/report/TradeData.java
+++ daily.trade.report/src/main/java/firoz/daily/trade/report/TradeData.java
@@ -13,4 +13,76 @@ public class TradeData {
 	int units;
 	double price;
 	
+
+	public TradeData() {
+		super();
+	}
+	public TradeData(String entity, String action, double agreedFx, String currency, Date instructionDate,
+			Date settlementDate, int units, double price) {
+		super();
+		this.entity = entity;
+		this.action = action;
+		this.agreedFx = agreedFx;
+		this.currency = currency;
+		this.instructionDate = instructionDate;
+		this.settlementDate = settlementDate;
+		this.units = units;
+		this.price = price;
+	}
+	
+	public String getEntity() {
+		return entity;
+	}
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
+	public double getAgreedFx() {
+		return agreedFx;
+	}
+	public void setAgreedFx(double agreedFx) {
+		this.agreedFx = agreedFx;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public Date getInstructionDate() {
+		return instructionDate;
+	}
+	public void setInstructionDate(Date instructionDate) {
+		this.instructionDate = instructionDate;
+	}
+	public Date getSettlementDate() {
+		return settlementDate;
+	}
+	public void setSettlementDate(Date settlementDate) {
+		this.settlementDate = settlementDate;
+	}
+	public int getUnits() {
+		return units;
+	}
+	public void setUnits(int units) {
+		this.units = units;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "TradeData [entity=" + entity + ", action=" + action + ", agreedFx=" + agreedFx + ", currency="
+				+ currency + ", instructionDate=" + instructionDate + ", settlementDate=" + settlementDate + ", units="
+				+ units + ", price=" + price + "]";
+	}
 }
